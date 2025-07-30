ce5c42678c5c35f95557cbca5ed661b52a59515b
solex-001 <adesolaadebayo280@gmail.com>
2025-07-30 09:30:28 +0100

    Add files via upload

---
+++ solex.js
@@ -0,0 +1,10 @@
+// Example
+
+var ourname ="Adesola Adebayo";
+var ourstr ="Hello, our name is" + ourname + ",how are you ? ";
+
+// only change code below this line
+
+var myname ="Adesola";
+var mystr = "my name is " + myname + "and i am well";
+console.log(mystr)
